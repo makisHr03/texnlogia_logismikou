5bbf48414e45e5032dd6d6940f8b95c0f4aa7b37
makis5051 <127298005+makisHr03@users.noreply.github.com>
2024-11-13 18:26:09 +0200

    Add files via upload

---
+++ music_festival/src/main/java/com/festival/music_festival/Performance/PerformanceRepository.java
@@ -0,0 +1,6 @@
+package com.festival.music_festival.Performance;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerformanceRepository extends JpaRepository<Performance, Long> {
+}
--- music_festival/src/main/java/com/festival/music_festival/Performance/Performance_service.java
+++ music_festival/src/main/java/com/festival/music_festival/Performance/Performance_service.java
@@ -1,20 +1,34 @@
 package com.festival.music_festival.Performance;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 @Service
 public class Performance_service {
+
+    private final PerformanceRepository performanceRepository;
+
+    @Autowired
+    public Performance_service(PerformanceRepository performanceRepository) {
+        this.performanceRepository = performanceRepository;
+    }
+
     public List<Performance> get_performance() {
-        return List.of(
-                new Performance(
-                        1L,
-                        "BTS",
-                        "K-pop",
-                        "Korea",
-                        LocalDate.of(2024, 1, 1),
-                        "aaa"));
+        return performanceRepository.findAll();
+    }
+
+    public void saveSamplePerformance() {
+        Performance samplePerformance = new Performance(
+                null, // ID will be auto-generated
+                "Music Fest",
+                "BTS",
+                "K-pop",
+                "Korea",
+                LocalDate.of(2024, 1, 1),
+                "AAA performance description");
+        performanceRepository.save(samplePerformance);
     }
 }
