b7704fef9464ad6f59ff1efe07f5e7b8779b6fa2
makis5051 <127298005+makisHr03@users.noreply.github.com>
2024-11-17 18:17:07 +0200

    Delete music_festival directory

--- music_festival/src/main/java/com/festival/music_festival/Performance/PerformanceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.festival.music_festival.Performance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerformanceRepository extends JpaRepository<Performance, Long> {
-}
